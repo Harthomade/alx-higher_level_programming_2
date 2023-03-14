1e47720d214e24e326a1a48e6cd66dcdc578f599
Harthomade <adeyinkatemitope53@gmail.com>
2023-03-13 17:24:43 -0700

    grown

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
- (6 sloc)  176 Bytes
-
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < len(my_list) and idx > -1:
-        my_list[idx] = element
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    return my_list
+    else:
+        my_list[idx] = element
+        return my_listt
