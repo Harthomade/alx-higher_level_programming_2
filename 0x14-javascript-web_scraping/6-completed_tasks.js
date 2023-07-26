4fd8a208e266512d1197fbb74d7d49b46cca945f
Harthomade <adeyinkatemitope53@gmail.com>
2023-07-25 17:52:35 -0700

    serialization is coverting object to string transmitted through a network

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,26 @@
 #!/usr/bin/node
 
-const url = process.argv[2];
 const request = require('request');
+const url = process.argv[2];
 
-request(url, (err, { statusCode, body }) => {
-  if (err) return console.log(err);
-  if (statusCode === 200) {
-    const dic = {};
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
     const tasks = JSON.parse(body);
-    tasks.every(({ userId, completed }) => {
-      if (completed) {
-        if (dic[userId]) return (dic[userId] += 1);
-        return (dic[userId] = 1);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
-      return true;
-    });
-    return console.log(dic);
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-  console.log(`Error code: ${statusCode}`);
 });
